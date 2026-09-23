cf1b60ef238d54a0583b5d8374fdc10dfd559f56
marekpal <marek@palczynski.com.pl>
2026-09-23 12:41:42 +0200

    translated in n8n

---
+++ test.text.pl
@@ -0,0 +1,4 @@
+  Tylko przykładowy plik do przetestowania n8n. Kolejne zdanie w tym samym akapicie.
+Nowy wiersz oznacza nowy akapit. Przetestuj jeszcze jedno zdanie w tym wierszu.
+Trzecia linijka. Przetestuj, czy to trzeci segment wysłany do przetłumaczenia w TextUnited.
+I przetestuj ostatni.
