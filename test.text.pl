7b614631af21108d0de786ab44f331a3990a6c70
marekpal <marek@palczynski.com.pl>
2026-09-23 12:42:48 +0200

    Delete test.text.pl

--- test.text.pl
+++
@@ -1,4 +0,0 @@
-  Tylko przykładowy plik do przetestowania n8n. Kolejne zdanie w tym samym akapicie.
-Nowy wiersz oznacza nowy akapit. Przetestuj jeszcze jedno zdanie w tym wierszu.
-Trzecia linijka. Przetestuj, czy to trzeci segment wysłany do przetłumaczenia w TextUnited.
-I przetestuj ostatni.
