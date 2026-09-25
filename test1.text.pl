f00e59b97ad83e010f06f1f982d90d5b6f1b6e9d
marekpal <marek@palczynski.com.pl>
2026-09-25 09:24:03 +0200

    translated in n8n

---
+++ test1.text.pl
@@ -0,0 +1,4 @@
+  Tylko przykładowy plik do testowania n8n. Kolejne zdanie w tym samym akapicie.
+Nowy wiersz oznacza nowy akapit. I jeszcze jedno zdanie w tym wierszu.
+Trzecia linijka. Powinien to być trzeci segment wysłany do przetłumaczenia w TextUnited.
+I ostatni.
